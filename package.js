7983fb41b4e1578f677daac985c25b2c4f4e4545
oluiz3515 <138527745+oluiz3515@users.noreply.github.com>
2025-08-14 01:14:23 -0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "roblox-discord-api",
+  "version": "1.0.0",
+  "description": "API para sincronizar cargos Discord ↔ Roblox",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mongoose": "^7.3.1",
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.3.1"
+  }
+}
